55025ad6df45bc46cd9c521635c651f537bb93bf
Deepak Gupta <76244467+deepakrvg@users.noreply.github.com>
2026-05-05 01:31:19 +0530

    Add: time-based-key-value-store - submission-6

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-6.cpp
@@ -0,0 +1,22 @@
+class TimeMap {
+public:
+    unordered_map<string, vector<pair<string, int>>> mp;
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({value, timestamp});
+    }
+    
+    string get(string key, int timestamp) {
+        if (mp.find(key) == mp.end()) return "";
+        vector<pair<string, int>>& temp = mp[key];
+        auto it = upper_bound(temp.begin(), temp.end(), timestamp, [](int timestamp, const pair<string, int>& ts) {
+            return timestamp < ts.second;
+        });
+        if (temp.size() == 0 || it == temp.begin()) return "";
+        it--;
+        return temp[it - temp.begin()].first;
+    }
+};
