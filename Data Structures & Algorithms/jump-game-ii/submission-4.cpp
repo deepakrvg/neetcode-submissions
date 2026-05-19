28fa6beb564979ae1205ffffb4c6edf91b0b588c
Deepak Gupta <76244467+deepakrvg@users.noreply.github.com>
2026-05-19 19:21:45 +0530

    Add: jump-game-ii - submission-4

---
+++ Data Structures & Algorithms/jump-game-ii/submission-4.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int r = 0;
+        int steps = 0;
+        int prev_r = 0;
+        for (int i = 0; i < n; ++i) {
+            if (prev_r < i) {
+                steps++;
+                prev_r = r;
+            }
+            r = max(r, i + nums[i]);
+        }
+        return steps;
+    }
+};
